64e4c37c17ea6faa78748b8e5d93328e610f1d87
mehdiaitouchrif <mehdi.aitouchrif@outlook.com>
2023-04-02 23:02:24 +0000

    reserve table functionality, and add favicon

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
 import "./App.css";
+import BookingPage from "./pages/BookingPage";
+import ConfirmedBooking from "./pages/ConfirmedBooking";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/reservations" element={<BookingPage />} />
+        <Route path="confirmed-booking" element={<ConfirmedBooking />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ReservationStateProvider } from "./context/ReservationContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ReservationStateProvider>
+      <App />
+    </ReservationStateProvider>
   </React.StrictMode>
 );
 
