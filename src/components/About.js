791680b0c16f88dc53ddb9bcca75690000b9c3c6
mehdiaitouchrif <mehdi.aitouchrif@outlook.com>
2023-04-02 17:20:46 +0000

    add about section

---
+++ src/components/About.js
@@ -0,0 +1,34 @@
+const About = () => {
+  return (
+    <div className="about">
+      <div className="container">
+        {/* Text */}
+        <div className="about-text">
+          <h2>Little Lemon</h2>
+          <h3>Chicago</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
+            expedita voluptatem asperiores sapiente eveniet excepturi? Dolore
+            aperiam doloribus ducimus natus porro, qui quam culpa quae corrupti
+            perspiciatis. Illum possimus blanditiis et in accusantium. Placeat,
+            hic! Tempore laboriosam dignissimos eum commodi! Lorem ipsum dolor
+            sit amet consectetur adipisicing elit. Est quibusdam nulla modi
+            eveniet voluptatem id. Earum vitae enim quisquam magni? Vero ipsa
+            hic quaerat voluptatibus illo laborum fuga alias nemo, deserunt sunt
+            voluptatem fugit vel et esse quisquam. Porro ex nihil magni nam
+            necessitatibus nemo, reiciendis distinctio ratione dignissimos fugit
+            officiis accusamus soluta sequi dolorem vitae, quos dicta cum et!
+          </p>
+        </div>
+
+        {/* Photos of Adrian & Mario */}
+        <div className="about-photos">
+          <img src="mario-and-adrian-1.jpg" alt="" />
+          <img src="mario-and-adrian-2.jpg" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,4 @@
+import About from "./About";
 import CallToAction from "./CallToAction";
 import Specials from "./Specials";
 import Testimonials from "./Testimonials";
@@ -8,6 +9,7 @@ const Main = () => {
       <CallToAction />
       <Specials />
       <Testimonials />
+      <About />
     </main>
   );
 };
